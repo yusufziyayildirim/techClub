4c2d9aaf7ca4cacba4a2958fa41be9ddcdf57d03
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-10-13 10:27:05 +0300

    Update Animation.js

--- coop-react/src/components/ui/Animation.js
+++ coop-react/src/components/ui/Animation.js
@@ -1,6 +1,9 @@
 import styled, { keyframes } from 'styled-components'
 import { fadeInUp } from 'react-animations';
+import { headShake } from 'react-animations';
 
+const AnimationShake = styled.div`animation: ${props => props.delay ? `${props.delay}s` : "1s"} ${keyframes`${headShake}`}`;
 const AnimationFade = styled.div`animation: ${props => props.delay ? `${props.delay}s` : "1s"} ${keyframes`${fadeInUp}`}`;
 
-export {AnimationFade}
+
+export {AnimationShake, AnimationFade}
