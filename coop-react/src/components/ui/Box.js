f2c639b6bd05f532dd8aabcfa9b422323456e8d6
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-10-13 10:27:43 +0300

    Code review

--- coop-react/src/components/ui/Box.js
+++ coop-react/src/components/ui/Box.js
@@ -1,25 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 import CounterUpPage from './CounterUpPage';
-import ScrollTrigger from 'react-scroll-trigger';
-import AnimationFade from './AnimationFade';
 
-const Box = ({ src, count, title, delay }) => {
-    const [scroll, setScroll] = useState(false);
+const Box = ({ src, count, title }) => {
 
     return (
-        <ScrollTrigger onEnter={() => setScroll(true)}>
-            {scroll && (
-                <AnimationFade delay={delay}>
-                    <div className='bg-[#0d0d0f] py-12 rounded-xl flex items-center justify-center -skew-y-6 hover:-skew-y-0 ease-out duration-500 group shadow-sm shadow-[#FF8D02]/80'>
-                        <div className='skew-y-6 group-hover:skew-y-0 ease-out duration-500 flex flex-col items-center font-saira'>
-                            <img className='h-[85px] mb-5' src={src} alt='code' />
-                            <CounterUpPage count={count}/>
-                            <h4 className='text-xl leading-8 font-bold mb-2 text-center text-[#e4e4e4] '>{title}</h4>
-                        </div>
-                    </div>
-                </AnimationFade>
-            )}
-        </ScrollTrigger >
+        <div className='bg-[#0d0d0f] py-12 rounded-xl flex items-center justify-center -skew-y-6 hover:-skew-y-0 ease-out duration-500 group shadow-sm shadow-[#FF8D02]/80'>
+            <div className='skew-y-6 group-hover:skew-y-0 ease-out duration-500 flex flex-col items-center font-saira'>
+                <img className='h-[85px] mb-5' src={src} alt='code' />
+                <CounterUpPage count={count} />
+                <h4 className='text-xl leading-8 font-bold mb-2 text-center text-[#e4e4e4] '>{title}</h4>
+            </div>
+        </div>
     )
 }
 
--- coop-react/src/components/ui/CounterUpPage.js
+++ coop-react/src/components/ui/CounterUpPage.js
@@ -1,16 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import CountUp from 'react-countup';
-import ScrollTrigger from 'react-scroll-trigger';
 
 const CounterUpPage = ({ count }) => {
-    const [counteron, setCounterOn] = useState(false);
     return (
-        <ScrollTrigger onEnter={() => setCounterOn(true)}>
-            <h1 className='font-bold text-4xl leading-[55px] text-[#e4e4e4]'>
-                {counteron && (<CountUp start={0} end={count} duration={2} delay={0} />)}
-                <sup>+</sup>
-            </h1>
-        </ScrollTrigger>
+        <h1 className='font-bold text-4xl leading-[55px] text-[#e4e4e4]'>
+            <CountUp start={0} end={count} duration={2} delay={0} />
+            <sup>+</sup>
+        </h1>
     )
 }
 
