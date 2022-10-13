246811eb1ee468185b0afc1b0b70c13cb35a7629
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-10-13 09:14:22 +0300

    About.js code review

--- coop-react/src/components/About.js
+++ coop-react/src/components/About.js
@@ -1,10 +1,8 @@
 import darkLogo from '../assets/image/darkLogo.png'
 import AnimationFade from './ui/AnimationFade'
-import ScrollTrigger from 'react-scroll-trigger';
-import { useState } from 'react';
+import Scroll from './ui/ScrollTrigger';
 
 const About = ({ styles, width }) => {
-    const [scroll, setScroll] = useState(false);
 
     return (
         <div>
@@ -14,41 +12,36 @@ const About = ({ styles, width }) => {
             {/* About content */}
             <div style={width < 475 && width * 0.0021 < 1 ? styles.scale : {}} className="!max-w-lg px-3 xs:px-0 relative mx-auto py-8 h-screen flex flex-col justify-around 2xl:scale-110">
                 {/* Title */}
-                <ScrollTrigger onEnter={() => setScroll(true)}>
-                    {scroll && (
-                        <AnimationFade delay={1.3}>
-                            <h1 className="text-5xl font-bold text-[#FF8D02] mb-2">About</h1>
-                        </AnimationFade>
-                    )}
-                </ScrollTrigger >
+                <Scroll >
+                    <AnimationFade delay={1.3}>
+                        <h1 className="text-5xl font-bold text-[#FF8D02] mb-2">About</h1>
+                    </AnimationFade>
+                </Scroll>
 
-                <ScrollTrigger onEnter={() => setScroll(true)}>
-                    {scroll && (
-                        <AnimationFade delay={2}>
+                {/* Description */}
+                <Scroll >
+                    <AnimationFade delay={1}>
+                        <p className='mb-3'>
+                            NetRD Bilgi Teknolojileri ve Telekomünikasyon A.S, is now part of Orion Innovation Group. Orion is a leading digital transformation and product development services firm. Headquartered in Edison, NJ, we have a global team of 6,000+ associates, with engineers in 13 major delivery centers across North America, Europe, Asia and Latin America.
 
-                            {/* Description */}
-                            <p className='mb-3'>
-                                NetRD Bilgi Teknolojileri ve Telekomünikasyon A.S, is now part of Orion Innovation Group. Orion is a leading digital transformation and product development services firm. Headquartered in Edison, NJ, we have a global team of 6,000+ associates, with engineers in 13 major delivery centers across North America, Europe, Asia and Latin America.
+                            For over 25 years, Orion has been solving complex business problems for our clients. Our transformative business solutions are rooted in digital strategy, experience design, and engineering, empowering our clients to operate with agility at scale.
 
-                                For over 25 years, Orion has been solving complex business problems for our clients. Our transformative business solutions are rooted in digital strategy, experience design, and engineering, empowering our clients to operate with agility at scale.
+                            Our mission is to serve as an agile and trusted partner for business transformation initiatives, providing deep emerging technology, experience design, and domain expertise.
 
-                                Our mission is to serve as an agile and trusted partner for business transformation initiatives, providing deep emerging technology, experience design, and domain expertise.
-
-                                Our business has more than tripled over the last three years. We have grown aggressively both organically and inorganically, adding new clients, complementary skills, domain expertise, and strengthening our global footprint.
-                            </p>
-
-                        </AnimationFade>
-                    )}
-                </ScrollTrigger >
-
+                            Our business has more than tripled over the last three years. We have grown aggressively both organically and inorganically, adding new clients, complementary skills, domain expertise, and strengthening our global footprint.
+                        </p>
+                    </AnimationFade>
+                </Scroll>
 
                 {/* Logo */}
-                <div className='w-full'>
-                    <img src={darkLogo} alt="logo" className='w-56 float-right' />
-                </div>
-
+                <Scroll >
+                    <AnimationFade delay={1}>
+                        <div className='w-full z-10'>
+                            <img src={darkLogo} alt="logo" className='w-56 float-right' />
+                        </div>
+                    </AnimationFade>
+                </Scroll>
             </div>
-
         </div>
     )
 }
